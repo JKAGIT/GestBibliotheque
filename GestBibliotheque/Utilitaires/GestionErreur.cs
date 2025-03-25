ad47deb76faf4bd015b69f7aa89f5ba453f86a91
Jules Amelin <Jules Amelin@momentum-tech.ca>
2025-03-25 10:09:17 -0400

    Journalisation

--- GestBibliotheque/Utilitaires/GestionErreur.cs
+++ GestBibliotheque/Utilitaires/GestionErreur.cs
@@ -2,32 +2,27 @@
 using Microsoft.Extensions.Logging;
 
 namespace GestBibliotheque.Utilitaires
-{  
+{
+
 
     public static class GestionErreurs
     {
         public static void GererErreur(Exception ex, Controller controller)
         {
-            string messageErreur;
+            string messageErreur = ex is ArgumentNullException || ex is InvalidOperationException || ex is KeyNotFoundException
+                ? ex.Message
+                : $"Une erreur inattendue est survenue. Détails : {ex.Message}";
 
-            if (ex is ArgumentNullException || ex is InvalidOperationException || ex is KeyNotFoundException)
+            if (!string.IsNullOrWhiteSpace(messageErreur))
             {
-                messageErreur = ex.Message;
+                // Singleton
+                //var logger = LoggerSingleton.Instance;  // Récupère l'instance de LoggerSingleton
+                //logger.LogError(messageErreur);  // Log l'erreur dans le fichier log
+
+                // methode statique
+                Logger.LogError(messageErreur);
             }
-            else
-            {
-                messageErreur = $"Une erreur inattendue est survenue. Détails : {ex.Message}";
-            }
-
-
-            //  -- A faire-----Journalisation
-            //if (!string.IsNullOrWhiteSpace(messageErreur))
-            //{
-            //   
-            //}
-
             controller.ModelState.AddModelError(string.Empty, messageErreur);
         }
     }
-
 }
