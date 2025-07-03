64ec097d65f57979d7ffea6f183203ce2c04647e
jules.amelin <jka1980@live.fr>
2025-07-03 13:24:23 -0400

    Adaptation program

--- GestBibliotheque/Program.cs
+++ GestBibliotheque/Program.cs
@@ -6,27 +6,23 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 using Serilog;
 
-
 var builder = WebApplication.CreateBuilder(args);
-// Configure Serilog
+
+// Pour les plateformes comme Render et Railway (port dynamique)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
+// Serilog pour la journalisation
 Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration) 
+    .ReadFrom.Configuration(builder.Configuration)
     .CreateLogger();
 builder.Host.UseSerilog();
 
-
-// Configure le port dynamiquement (utile pour Render et Railway)
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(int.Parse(port));
-});
-/////////////////////
-
-
+// Base de données
 builder.Services.AddDbContext<GestBibliothequeDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("GestBibliothequeDbConnect")));
 
+// Injections de dépendances
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IEntityValidationService<>), typeof(EntityValidationService<>));
@@ -34,34 +30,31 @@ builder.Services.AddScoped(typeof(IRecherche<>), typeof(Recherche<>));
 builder.Services.AddScoped<ICategories, CategoriesService>();
 builder.Services.AddScoped<IEmprunts, EmpruntsService>();
 builder.Services.AddScoped<ILivres, LivresService>();
-builder.Services.AddScoped<IReservations,ReservationsService>();
+builder.Services.AddScoped<IReservations, ReservationsService>();
 builder.Services.AddScoped<IUsagers, UsagersService>();
-builder.Services.AddScoped<IUtilisateurs,UtilisateursService>();
+builder.Services.AddScoped<IUtilisateurs, UtilisateursService>();
 builder.Services.AddScoped<IRetours, RetoursService>();
-
-
 builder.Services.AddScoped<GenerateurMatriculeUnique>();
 
+// Authentification Azure
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
 
-
-// Add services to the container.
+// MVC
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuration de l'app
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+
+    // Ne pas rediriger vers HTTPS en Docker (Render/Railway)
+    // app.UseHttpsRedirection();
+    // app.UseHsts();
 }
 
-
-
-app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthentication();
@@ -75,3 +68,76 @@ app.MapControllerRoute(
     .WithStaticAssets();
 
 app.Run();
+
+
+
+
+//Version avant render et railway
+//using GestBibliotheque.Donnee;
+//using GestBibliotheque.Repositories;
+//using GestBibliotheque.Services;
+//using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Identity.Web;
+//using Serilog;
+
+
+//var builder = WebApplication.CreateBuilder(args);
+//// Configure Serilog
+//Log.Logger = new LoggerConfiguration()
+//    .ReadFrom.Configuration(builder.Configuration)
+//    .CreateLogger();
+//builder.Host.UseSerilog();
+
+
+//builder.Services.AddDbContext<GestBibliothequeDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("GestBibliothequeDbConnect")));
+
+//builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+//builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+//builder.Services.AddScoped(typeof(IEntityValidationService<>), typeof(EntityValidationService<>));
+//builder.Services.AddScoped(typeof(IRecherche<>), typeof(Recherche<>));
+//builder.Services.AddScoped<ICategories, CategoriesService>();
+//builder.Services.AddScoped<IEmprunts, EmpruntsService>();
+//builder.Services.AddScoped<ILivres, LivresService>();
+//builder.Services.AddScoped<IReservations, ReservationsService>();
+//builder.Services.AddScoped<IUsagers, UsagersService>();
+//builder.Services.AddScoped<IUtilisateurs, UtilisateursService>();
+//builder.Services.AddScoped<IRetours, RetoursService>();
+
+
+//builder.Services.AddScoped<GenerateurMatriculeUnique>();
+
+//builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+//    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+
+
+//app.UseHttpsRedirection();
+//app.UseRouting();
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+//app.MapStaticAssets();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}")
+//    .WithStaticAssets();
+
+//app.Run();
